187ae1c74ba188e79d43916390c20e5ad8240078
Mean <mean@Mean.local>
2020-03-30 22:57:43 +0800

    完成记账页面基本样式

--- miniprogram/pages/bookkeep/bookkeep.js
+++ miniprogram/pages/bookkeep/bookkeep.js
@@ -5,62 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 键盘
+    num: 0,
+    hasDot: false // 防止用户多次输入小数点
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  tapKey: function (evt) {
+    var x = evt.currentTarget.dataset.key
+    if (x == '.') {
+      if (this.data.hasDot) return
+      this.setData({
+        hasDot: true
+      })
+    }
+    this.setData({
+      num: this.data.num == '0' ? x : this.data.num + x
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  tapSubmit: function () {
+    // 用户已提交
+    console.log('res =', this.data.num)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  tapDel: function () {
+    if (this.data.num == '0') return
+    if (this.data.num[this.data.num.length - 1] == '.') this.setData({
+      hasDot: false
+    })
+    this.setData({
+      num: this.data.num.length == 1 ? '0' : this.data.num.substring(0, this.data.num.length - 1)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  tapClear: function () {
+    this.setData({
+      num: '0',
+      hasDot: false
+    })
   }
 })
