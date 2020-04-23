eaa1d8ba9d0c94ca6aaacae5ec373da721724807
Mean。 <mean325@qq.com>
2020-04-23 18:20:46 +0800

    加载界面修改计划

--- miniprogram/pages/welcome/welcome.js
+++ miniprogram/pages/welcome/welcome.js
@@ -1,13 +1,31 @@
+var amap = require('../../utils/amap-wx.js');   // 引入高德地图
+
 const app = getApp()
 
 Page({
   mixins: [require('../../libs/Mixins.js')],
   data: {
     canIUse: wx.canIUse('button.open-type.getUserInfo'),    // 判断小程序的API，回调，参数，组件等是否在当前版本可用
-    userInfo: {}    // 用户信息,在获取到openid后,保存到全局变量
+    userInfo: {},    // 用户信息,在获取到openid后,保存到全局变量
+    weather: {},    // 天气数据
   },
   onLoad(options) {
+    this.getWeather();
     this.getSetting();
+  },
+  getWeather() {
+    var amapFun = new amap.AMapWX({ key: 'a4a2d92da0b8a0b8ecbe699ef4e76426' });
+    amapFun.getWeather({
+      success: res => {
+        console.log(res)
+        this.setData({
+          weather: res.liveData
+        })
+      },
+      fail: err => {
+        console.log(err)
+      }
+    })
   },
   /**
    * 当userInfo可以被获取时,跳转到首页
