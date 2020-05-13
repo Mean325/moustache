ea19e18e9e2b0f16f0885dc5b105aa56c003a7a9
Mean。 <mean325@qq.com>
2020-05-13 20:59:45 +0800

    AA记账用户-团队管理表创建

---
+++ cloudfunctions/AA_addItem/index.js
@@ -0,0 +1,37 @@
+// AA记账 - 添加/编辑团队
+
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init({
+  traceUser: true,
+})
+const db = cloud.database()
+const _ = db.command
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext();
+  const { name } = event;
+
+  const res = await db.collection('AA_item').add({
+    data: {
+      createTime: new Date().getTime(),
+      name
+    }
+  });
+  if (res._id) {
+    console.log('添加团队成功')
+    console.log(res);
+    await db.collection('AA_user_item').add({
+      data: {
+        _openid: wxContext.OPENID,
+        _itemId: res._id
+      }
+    })
+      .then(resq => {
+        console.log('用户与团队管理表创建成功')
+        console.log(resq);
+        return res;
+      })
+  }
+}
--- miniprogram/pages/tools/AA/teamList/teamList.js
+++ miniprogram/pages/tools/AA/teamList/teamList.js
@@ -1,11 +1,15 @@
-// miniprogram/pages/tools/AA/team/team.js
-Page({
+const utils = require("../../../../../utils/utils.js");
+const app = getApp();
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    isDialogShow: false,
+    form: {
+      name: ''
+    }
   },
 
   /**
@@ -28,10 +32,61 @@ Page({
   onShow: function () {
 
   },
-  test() {
-    wx.showModal({
-      title: '',
-      content: '',
+  showModal() {
+    this.setData({
+      isDialogShow: true
     })
-  }
+  },
+  tapDialogButton(e) {
+    console.log(e);
+    if (e.detail.index === 1) {
+      console.log("创建");
+      this.addItem();
+    }
+    this.setData({
+      isDialogShow: false
+    })
+  },
+  /**
+ * 实现数据双向绑定
+ * @hook 顶部分类名称input输入事件
+ */
+  handleInputChange(e) {
+    this.setData({
+      'form.name': e.detail.value
+    })
+  },
+  /**
+   * 调用云函数AA_addItem新增用户默认分类
+   * @method 底部添加分类事件
+   */
+  addItem: utils.throttle(function () {
+    let pages = getCurrentPages();
+    let { options } = pages.pop();
+    let type = Number(options.type);   // 获取路由参数type
+
+    let { name } = this.data.form;
+    if (!name) {
+      wx.showToast({
+        title: '请输入团队名称',
+        icon: 'none',
+      })
+      return;
+    }
+    wx.cloud.callFunction({
+      name: 'AA_addItem',
+      data: {
+        name
+      }
+    })
+      .then(res => {
+        console.log(res);
+        wx.showToast({
+          title: '创建成功',
+          icon: 'none',
+        })
+        // wx.navigateBack();    // 返回上一页
+      })
+      .catch(console.error)
+  }, 1000)
 })
